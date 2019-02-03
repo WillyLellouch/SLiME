1894429df77de17f99cfcfd1dbb6e533ac55ac5b
Aurélien Tauvy <aurelien.tauvy@etu.unilim.fr>
2019-02-03 12:55:58 +0100

    Begin photon
    CAREFUL : Not tested

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class GameManager : Photon.PunBehaviour {
+
+    #region Photon Messages
+    
+    //called when the local player left the room. We need to load the launcher scene.
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+    #region Public Methods
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+
+    #region Photon Messages
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerConnected() " + other.NickName);
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected is MasterClient " + PhotonNetwork.isMasterClient);
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorManager : Photon.MonoBehaviour{
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(photonView.isMine == false && PhotonNetwork.connected == true)
+        {
+            return;
+        }
+	}
+}
