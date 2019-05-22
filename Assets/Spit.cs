5eaba4d60f29c469ddbe48246ce55ed15287f1ab
Nelyanen <dreamer.chrysalis@gmail.com>
2019-05-22 14:06:11 +0200

    #19 Mise en place du script pour les projectiles

---
+++ Assets/Spit.cs
@@ -0,0 +1,61 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spit : MonoBehaviourPun
+{
+    public float speed = 10;
+
+    public int damage = 3;
+
+    public float lifecycle = 1f;
+
+    private float despawnTime;
+
+    private Rigidbody spitRigidBody;
+
+    private SphereCollider spitCollider;
+
+    [HideInInspector]
+    public GameObject owner;
+
+    void Awake()
+    {
+        spitRigidBody = GetComponent<Rigidbody>();
+        spitCollider = GetComponent<SphereCollider>();
+        OnSpawn();
+    }
+
+    void OnSpawn()
+    {
+        spitRigidBody.velocity = speed * transform.forward;
+        if(lifecycle > 0)
+        {
+            despawnTime = Time.time + lifecycle;
+        }
+        else
+        {
+            despawn();
+        }
+    }
+
+    void Update()
+    {
+        if(Time.time > despawnTime)
+        {
+            despawn();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        despawn();
+    }
+
+    void despawn()
+    {
+        Destroy(this);
+    }
+}
