27bc69e4b08770e6a222d139172cce7b9c8614fd
Nelyanen <dreamer.chrysalis@gmail.com>
2019-06-16 15:56:31 +0200

    #10 bugfix

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -35,19 +35,6 @@ public class GameManager : MonoBehaviourPunCallbacks
     }
 
     #endregion
-        if (PlayerManager.LocalPlayerInstance == null)
-        {
-            Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
-            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
-        }
-        else
-        {
-            Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
-        }
-    }
-
-    #endregion
 
 
     #region Photon Callbacks
